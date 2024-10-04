9921904ff64bcc9aa7ab5361c0b47ca903eab620
yeosang0508 <yeosang0508@gmail.com>
2024-10-04 12:56:15 +0900

    프로젝트 생성 및 환경세팅

---
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -0,0 +1,13 @@
+package com.project.tailsroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTailsrouteApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(ProjectTailsrouteApplication.class, args);
+	}
+}
