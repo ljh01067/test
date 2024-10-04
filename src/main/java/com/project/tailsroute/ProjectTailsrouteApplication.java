51da3422bb972756d858ed4908247d29bf57e869
yeosang0508 <yeosang0508@gmail.com>
2024-10-04 21:20:36 +0900

    test

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectTailsrouteApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
 }
