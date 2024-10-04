92b4d09e0c87667894e1a2a1776eec2294830538
Eichi2514 <picon1317@gmail.com>
2024-10-04 23:32:06 +0900

    thymeleaf 기본셋팅

---
+++ src/main/java/com/project/tailsroute/controller/UsrHomeController.java
@@ -0,0 +1,16 @@
+package com.project.tailsroute.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsrHomeController {
+    @GetMapping("/usr/home/main")
+    public String showMain(Model model) {
+        model.addAttribute("message", "연결성공");
+        return "usr/home/main";
+
+    }
+}
