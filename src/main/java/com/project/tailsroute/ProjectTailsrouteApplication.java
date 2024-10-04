e98a58a3a3d8252830d23bf1b18d16103a695d1c
ljh01067 <leejh01067@gmail.com>
2024-10-04 13:42:47 +0900

    ljhtest

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectTailsrouteApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello ljh");
+		System.out.println("Hello");
 		System.out.println("test JBY");
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
