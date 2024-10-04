f9f1581b1e507a446e995a9e29ed7fe6c989bea3
jby9725 <jby9725@gmail.com>
2024-10-05 00:06:30 +0900

    인사빌런

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -10,8 +10,7 @@ public class ProjectTailsrouteApplication {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요 조보영입니다.");
 		System.out.println("안녕하세요 조보영입니다. DB 설정도 좀 만졌습니다.");
-
-
+		System.out.println("안녕하세요 인사빌런입니다.");
 
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
