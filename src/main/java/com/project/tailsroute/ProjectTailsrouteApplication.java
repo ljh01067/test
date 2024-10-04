aaa4118ba919ec16a21c815396158bf323ed4c4e
jby9725 <jby9725@gmail.com>
2024-10-04 23:32:58 +0900

    DB setting, second commit

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -9,6 +9,10 @@ public class ProjectTailsrouteApplication {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요 조보영입니다.");
+		System.out.println("안녕하세요 조보영입니다. DB 설정도 좀 만졌습니다.");
+
+
+
 		SpringApplication.run(ProjectTailsrouteApplication.class, args);
 	}
 }
