3d36db039b59a5e8873cf3b9c65f134b95b56649
Eichi2514 <picon1317@gmail.com>
2024-10-14 11:50:55 +0900

    로그인 안하면 실종리스트 못보는 문제 해결

--- src/main/java/com/project/tailsroute/controller/UsrHomeController.java
+++ src/main/java/com/project/tailsroute/controller/UsrHomeController.java
@@ -2,6 +2,7 @@ package com.project.tailsroute.controller;
 
 import com.project.tailsroute.service.MemberService;
 //import com.project.tailsroute.service.MissingService;
+import com.project.tailsroute.service.MissingService;
 import com.project.tailsroute.vo.Member;
 import com.project.tailsroute.vo.Missing;
 import com.project.tailsroute.vo.Rq;
@@ -20,8 +21,8 @@ public class UsrHomeController {
         this.rq = rq;
     }
 
-//    @Autowired
-//    private MissingService missingService;
+    @Autowired
+    private MissingService missingService;
 
     @GetMapping("/usr/home/main")
     public String showMain(Model model) {
@@ -32,10 +33,10 @@ public class UsrHomeController {
             model.addAttribute("member", member);
         }
 
-//        List<Missing> missings = missingService.list(0, 10, "전체");
+        List<Missing> missings = missingService.list(0, 10, "전체");
 
         model.addAttribute("isLogined", isLogined);
-//        model.addAttribute("missings", missings);
+        model.addAttribute("missings", missings);
         return "usr/home/main";
     }
 }
